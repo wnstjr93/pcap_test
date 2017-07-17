60578525bb07b4233c937b3928523e9a9d636ea6
wnstjr93 <kjsgood21@gmail.com>
2017-07-17 18:32:35 +0900

    final??

--- pcap_test.c
+++ pcap_test.c
@@ -2,25 +2,35 @@
 #include <stdio.h>
 
 int Mac_ad(const u_char *packet);
-int Ip_ad(const u_char *packet,int i);
-
+int Ip_ad(const u_char *packet);
+int Port_ad(const u_char *packet,int i);
 
 typedef struct ether_info
-	{
-		u_char Mac_dst[6];
-		u_char Mac_src[6];
-		short ether_type;
-	}ether_info;
+{
+	u_char Mac_dst[6];
+	u_char Mac_src[6];
+	short ether_type;
+}ether_info;
 
 typedef struct ip_info
-	{
-		u_char Ip_version:4;
-		u_char Head_len:4;
-		short Ip_len;
-		u_char Ip_dst[4];
-		u_char Ip_src[4];
-	}ip_info;
+{
+	u_char Ip_version:4;
+	u_char Head_len:4;
+	u_char Tos;
+	short Ip_len;
+	short Iden;
+	short frag;
+	u_char unused[4];
+	u_char Ip_src[4];
+	u_char Ip_dst[4];
+}ip_info;
 
+typedef struct Port_info
+{
+	short Port_src;
+	short Port_dst;
+	u_char unused[16];
+}Port_info;
 
 
 int main(int argc, char *argv[])
@@ -35,7 +45,7 @@ int main(int argc, char *argv[])
 	struct pcap_pkthdr *header;	/* The header that pcap gives us */
 	const u_char *packet;		/* The actual packet */
 	int i;				/* ether->ip->tcp->http flag */
-	
+
 	/* Define the device */
 	dev = pcap_lookupdev(errbuf);
 	if (dev == NULL) {
@@ -67,16 +77,17 @@ int main(int argc, char *argv[])
 		return(2);
 	}
 	/* Grab a packet */ //1000=1
-//	while(1) {
+	//while(1) {
 		pcap_next_ex(handle, &header ,&packet) ; //data in packet
-		
-		
-		
 		for(i=0;i<header->len;i++){
-		if(i%8==0||i==header->len)printf("\n");
-		printf("%02x ",packet[i]);}
-		i=Mac_ad(packet);
-		Ip_ad(packet,i);
+			if(i%8==0||i==header->len)printf("\n");
+			printf("%02x ",packet[i]);
+		}
+		
+		
+		Mac_ad(packet);
+		i=Ip_ad(packet);
+		Port_ad(packet,i);
 		/* And close the session */
 //	}
 	pcap_close(handle);
@@ -92,38 +103,47 @@ int Mac_ad(const u_char *packet)
 	printf("\n******ether packet******\n");
 	for(i = 0; i < 12; i++)
 	{
-	if(i==0)printf("Dst Mac_Adress : %02x ",ether->Mac_dst[i]);	
-	else if(i==6) printf("\nSrc Mac_Adress : %02X ",ether->Mac_src[i]);
-	else printf(": %02x ",packet[i]);
+		if(i==0)printf("Dst Mac_Adress : %02x ",ether->Mac_dst[i]);	
+		else if(i==6) printf("\nSrc Mac_Adress : %02X ",ether->Mac_src[i]);
+		else printf(": %02x ",packet[i]);
 	}
 	M_ether_type=ntohs(ether->ether_type);//big->little endian
 	printf("\nnext protocol :%03x\n",M_ether_type);
-	printf("******ether packet******\n");
-	return i+10;
+	printf("************************\n");
+	return 0;//i=i+12;// wait..
 
 }
 /*IP_address - Ip part*/
-int Ip_ad(const u_char *packet,int i)
+int Ip_ad(const u_char *packet)
 {
-	int k=i+8;
 	ip_info *ip;
-	ip=(ip_info *)packet;
-	printf("\n*********ip packet******\n");
-	for(i;i<k;i++)
+	ip=(ip_info *)(packet+14);//structure point
+	u_char Header_len;
+	printf("\n*********ip packet*********\n");
+	
+	for(int i=0;i<8;i++)
 	{
-	if(i==(k-8))printf("Dst Ip_Address : %3d",ip->Ip_dst[i]);
-	else if(i==(k-4)) printf("\nSrc Ip_Address : %3d ",ip->Ip_src[i]);
-	else printf(".%3d ",packet[i]);
+		if(i==0)printf("Src Ip_Address : %3d",ip->Ip_src[i]);//
+		else if(i==4)printf("\nDst Ip_Address : %3d",ip->Ip_dst[i-4]);///
+		else if(i>0&&i<4)printf(". %d",ip->Ip_src[i]);
+		else printf(". %3d",ip->Ip_dst[i-4]);
 	}
-	i=k-8;
-	for(i;i<k;i++)
-	{
-	printf("%02x ",ip->Ip_dst[i]);
-	}
-	printf("\n*********ip packet******\n");
-	return i;
+	Header_len=5*(ip->Head_len);
+	printf("\n************************\n");
+
+	return Header_len;
 
 }
-/*(if[12]==0x08)&&(p[13]==0x00)*/
-
-
+int Port_ad(const u_char *packet, int i)
+{
+	Port_info *Port;
+	Port=(Port_info *)(packet+i+14);
+	printf("\n*********Port  address******\n");
+	unsigned short Ps=ntohs(Port->Port_src);
+	unsigned short Pd=ntohs(Port->Port_dst);
+	
+		printf("Src Port : %d\n",Ps);
+		printf("Dst Port : %d\n",Pd);
+	printf("****************************\n");
+	return 0;
+}
